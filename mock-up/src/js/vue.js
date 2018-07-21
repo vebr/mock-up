b311442ee6ec91c32b5be2d5f47ed01c63192ee6
veddhaedsa <skye420@keemail.me>
2018-07-22 02:02:55 +0700

    Informasi data edit updated

--- mock-up/src/js/vue.js
+++ mock-up/src/js/vue.js
@@ -1,81 +1,64 @@
-const informasiAkun = new Vue({
-	el: '#informasiAkun',
-	data: {
-		isEditing:false,
-		user: {
-			username: {
-				isEditing: false,
-				value: 'veddhaedsa'
-			},
-			nama: {
-				isEditing: false,
-				value: 'veddha edsa bening riady'
-			},
-			email: {
-				isEditing: false,
-				value: 'vedhaedsa@gmail.com'
-			},
-			password: {
-				isEditing: false,
-				value: 'Test12345'
-			},
-			statusPengguna: {
-				isEditing: false,
-				value: 'Siswa'
-			},
-		  },
-	},
-	mounted() {
-		this.cachedNama = Object.assign({}, this.user.nama);
-		this.cachedEmail = Object.assign({}, this.user.email);
-		this.cachedPassword = Object.assign({}, this.user.password);
-		this.cachedStatus = Object.assign({}, this.user.statusPengguna);
-	  },
-	methods: {
-		save() {
+var app = new Vue({
+		el: '#app',
+		data: {
+			user: {
+				username: {
+					isEditing: false,
+					value: 'veddhaedsa'
+				},
+				nama: {
+					isEditing: false,
+					value: 'veddha edsa bening riady'
+				},
+				email: {
+					isEditing: false,
+					value: 'vedhaedsa@gmail.com'
+				},
+				password: {
+					isEditing: false,
+					value: 'Test12345'
+				},
+				statusPengguna: {
+					isEditing: false,
+					value: 'Siswa'
+				},
+				},
+		},
+		mounted() {
+			this.cachedUser = Object.assign({}, this.user.username);
 			this.cachedNama = Object.assign({}, this.user.nama);
 			this.cachedEmail = Object.assign({}, this.user.email);
 			this.cachedPassword = Object.assign({}, this.user.password);
-			this.cachedStatus = Object.assign({}, this.user.statusPengguna);
-			this.user.nama.value = this.$refs['nama'].value;
-			this.user.nama.isEditing = false;
-			this.user.email.value = this.$refs['email'].value;
-			this.user.email.isEditing = false;
-			this.user.statusPengguna.value = this.user.statusPengguna.value;
-			this.user.statusPengguna.isEditing = false;
+			this.cachedSP = Object.assign({}, this.user.statusPengguna);
 		},
-		cancel() {
-			this.user.nama = Object.assign({}, this.cachedNama);
-			this.user.nama.isEditing = false;
-			this.user.email = Object.assign({}, this.cachedEmail);
-			this.user.email.isEditing = false;
-			this.user.password = Object.assign({}, this.cachedPassword);
-			this.user.password.isEditing = false;
-			this.user.statusPengguna = Object.assign({}, this.cachedStatus);
-			this.user.statusPengguna.isEditing = false;
+		methods: {
+			save() {
+				this.cachedUser = Object.assign({}, this.user.username);
+				this.user.username.isEditing = false;
+				this.cachedNama = Object.assign({}, this.user.nama);
+				this.user.nama.isEditing = false;
+				this.cachedEmail = Object.assign({}, this.user.email);
+				this.user.email.isEditing = false;
+				this.cachedPassword = Object.assign({}, this.user.password);
+				this.user.password.isEditing = false;
+				this.cachedSP = Object.assign({}, this.user.statusPengguna);
+				this.user.statusPengguna.isEditing = false;
+			},
+			cancel() {
+				this.user.username = Object.assign({}, this.cachedUser);
+				this.user.username.isEditing = false;
+				this.user.nama = Object.assign({}, this.cachedNama);
+				this.user.nama.isEditing = false;
+				this.user.email = Object.assign({}, this.cachedEmail);
+				this.user.email.isEditing = false;
+				this.user.password = Object.assign({}, this.cachedPassword);
+				this.user.password.isEditing = false;
+				this.user.statusPengguna = Object.assign({}, this.cachedSP);
+				this.user.statusPengguna.isEditing = false;
+			}
 		}
-	  }
-  });
- 
-  
-Vue.component('edit-informasiAkun', {
-	template: '#informasiAkun'
-  });
-Vue.component('edit-biodata', {
-	template: '#biodata'
-  })
-Vue.component('edit-pendidikan', {
-	template: '#pendidikan'
-  })
-  
-const tab = new Vue({
-	el: '#profile', 
-	data: {
-	  view: 'edit-informasiAkun', 
-	  active: 'informasiAkun',
-	}
-});
- // View password function
+	})
+// View password function
 function viewPassword() {
     var x = document.getElementById("password");
     if (x.type === "password") {
